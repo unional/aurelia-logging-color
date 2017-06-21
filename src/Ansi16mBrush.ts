322ae429015aedd1780c86f9985c8f306ca40bc7
Homa Wong <homawong@gmail.com>
2017-06-20 21:33:40 -0700

    Update Ansi16mBrush.ts
    Update styling

--- src/Ansi16mBrush.ts
+++ src/Ansi16mBrush.ts
@@ -1,14 +1,14 @@
-import {RGB, createColorsFromMap} from 'color-map'
+import { RGB, createColorsFromMap } from 'color-map'
 
 import { rainbow } from './colors'
 import { BrushOption, Brush } from './interfaces'
 
 export class Ansi16mBrush implements Brush {
-  public color: (id: string, ...rest: any[]) => string[];
-  private count = 0;
-  private colors: RGB[];
-  private map: { [index: string]: RGB } = {};
-  private option: BrushOption;
+  public color: (id: string, ...rest: any[]) => string[]
+  private count = 0
+  private colors: RGB[]
+  private map: { [index: string]: RGB } = {}
+  private option: BrushOption
 
   constructor(option: Partial<BrushOption> = {}) {
     this.option = {
@@ -23,7 +23,7 @@ export class Ansi16mBrush implements Brush {
         index: m.index,
         rgb: [m.rgb[0], m.rgb[1] * 0.7, m.rgb[2]] as RGB
       }
-    });
+    })
 
     this.colors = createColorsFromMap(colormap, option.maxColor || 20)
 
@@ -36,15 +36,15 @@ export class Ansi16mBrush implements Brush {
     return this.map[text] = this.map[text] || this.colors[this.count++ % this.option.maxColor]
   }
   private colorAnsi16m(id: string, ...rest: string[]) {
-    const rgb = this.getRgb(id);
+    const rgb = this.getRgb(id)
     return [this.wrapAnsi16m(id, rgb), ...rest]
   }
 
   private getAnsi16mBackgroundString(id: string, ...rest: string[]) {
-    const rgb = this.getRgb(id);
+    const rgb = this.getRgb(id)
     return [this.wrapAnsi16m(` ${id} `, rgb, 10), ...rest]
   }
   private wrapAnsi16m(id: string, rgb, offset: number = 0) {
-    return `\u001B[${38 + offset};2;${rgb[0]};${rgb[1]};${rgb[2]}m` + id + `\u001B[${39 + offset}m`
+    return `\u001B[${38 + offset}2${rgb[0]}${rgb[1]}${rgb[2]}m` + id + `\u001B[${39 + offset}m`
   }
 }
